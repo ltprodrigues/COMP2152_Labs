d9a5a01390dee39e3d401f3b2b21f22f0e2b666f
ltprodrigues <leticiatrodriguess@gmail.com>
2026-01-16 13:56:01 -0500

    lab 1 questions completed

---
+++ Week01/lab1.py
@@ -0,0 +1,27 @@
+# Sample Coding Question 01 Week 01
+# Leticia Rodrigues
+
+# Question 2: defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (a - ((b ** c) // d)) + (a % c)
+print("Question 3 result:", e)
+
+# Question 4: Formatting
+
+temperature = 32.6
+print("The temperature today is {:.3f} degrees Celsius".format(temperature))
+
+# Question 5: user input
+
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+print("Now showing the shop items filtered by age", userAge)
+
+
