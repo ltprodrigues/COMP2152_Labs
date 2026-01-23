bbd0fdf31b882869a561fd870f09d74a8b4271a2
ltprodrigues <leticiatrodriguess@gmail.com>
2026-01-23 11:32:40 -0500

    add w02 solution

--- Week02/Lab02.py
+++ Week02/Lab02.py
@@ -1,1 +1,31 @@
 # Solution for the week 02 Lab 
+
+#built in package
+import random
+
+choices = ["Rock", "Paper", "Scissors"]
+
+
+playerChoice = input("Enter a number between 1 to 3 for the following choices: 1. Rock 2. Paper 3. Scissors: ")
+
+playerChoice = int(playerChoice)
+
+if playerChoice < 1 or playerChoice > 3:
+    print("Error: Choice should be a number between 1 and 3.")
+else:
+    # Develop the game logic using if-elif-else statements
+    computerChoice = random.randrange(1, 3)
+
+    if playerChoice == computerChoice:
+        print("It's a tie!")
+    elif playerChoice == 1 and computerChoice == 3:
+        print("Rock beats Scissors! - You win!")
+    elif playerChoice == 2 and computerChoice == 1:
+        print("Paper beats Rock! - You win!")
+    elif playerChoice == 3 and computerChoice == 2:
+        print("Scissors beats Paper! - You win!")
+    else:
+        print("You lose!")
+
+        
+            
