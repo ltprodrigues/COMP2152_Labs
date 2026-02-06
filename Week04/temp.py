c9450a2140d745101eb4697dcd4750da0acf768e
ltprodrigues <leticiatrodriguess@gmail.com>
2026-02-06 11:44:25 -0500

    add w03

---
+++ Week04/temp.py
@@ -0,0 +1,13 @@
+#def function
+
+#def myFunction():
+#    pass
+#myFunction()
+
+def sum_a_b(a=0, b=0): #define arguments
+    print (a+b)
+    return a + b
+
+
+result = sum_a_b() #put it inside a var and print the result
+print(result)
